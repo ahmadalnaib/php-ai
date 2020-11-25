a5ad1290dc8a8de574bb6ffd0d6fe35c2ad81aca
ahmedhabeeb73 <alnaib888@gmail.com>
2020-11-25 12:35:08 +0100

    add images html

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+ <h1>all robots</h1>
+ <a target="_blank" href="assets/images/faheem.png">
+ <img src="assets/images/faheem.png" alt="">
+ <p><strong>faheem</strong></p>
+ </a>
+
+ <a target="_blank" href="assets/images/coder.png">
+ <img src="assets/images/coder.png" alt="">
+ <p><strong>coder</strong></p>
+ </a>
+ <a target="_blank" href="assets/images/sportsman.png">
+ <img src="assets/images/sportsman.png" alt="">
+ <p><strong>sportsman</strong></p>
+ </a>
+ <a target="_blank" href="assets/images/chemist.png">
+ <img src="assets/images/chemist.png" alt="">
+ <p><strong>chemist</strong></p>
+ </a>
+</body>
+</html>

2d9d45629116a66f864069ea38296fb16c2cb886
ahmedhabeeb73 <alnaib888@gmail.com>
2020-11-27 18:32:36 +0100

    add layouts folder

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <title>Document</title>
-</head>
-<body>
+
 
 <?php
+require_once('./layouts/header.php');
 include_once './classes/robot.class.php';
 
 $ahmed=new Robot('ahmed','php-laravel','faheem.png',array('what is noveLaravel',"node or laravel"),array('laravel admin','laravel'));
@@ -20,85 +12,50 @@ $zozo=new Robot('zozo','react-php','sportsman.png',array('what is express',"node
 
 $bobo=new Robot('zozo','react-php','sportsman.png',array('what is express',"node or c#"),array('node frame work','c#'));
 
+$bobo1=new Robot('zozo1','react-php','sportsman.png',array('what is express',"node or c#"),array('node frame work','c#'));
 
-$robots=[$ahmed,$zozo,$bobo];
+
+$robots=[$ahmed,$zozo,$bobo,$bobo1];
 
 $isLoggedIn=false;
 
 ?>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         اضف روبوتك
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-         
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+<main>
 
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
- 
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
- <hr>
-<div class="container">
-<div class="grid-main">
-
-  <?php foreach($robots as $robot): ?>
-    <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="<?php echo "assets/images/".$robot->image  ?>" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-dark"><?php echo $robot->name ?></h5>
-      <p class="card-text  text-dark"><?php echo $robot->spec ?></p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+   
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  
+   <hr>
+  <div class="container">
+  <div class="grid-main">
+  
+    <?php foreach($robots as $robot): ?>
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="<?php echo "assets/images/".$robot->image  ?>" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title text-dark"><?php echo $robot->name ?></h5>
+        <p class="card-text  text-dark"><?php echo $robot->spec ?></p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    <?php endforeach; ?>
   </div>
-  <?php endforeach; ?>
-</div>
+    </div>
+</main>
 
-        
-          </div>
+<hr>
 
-
-
-
-      
-
-
-
- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php require_once('./layouts/footer.php') ?>
